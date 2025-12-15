349818cabd31f8f9b20275c24717913461325af8
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2025-12-15 15:41:23 +0900

    minor fix

--- vep_cache/install_cache.sh
+++ vep_cache/install_cache.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export CACHE_DIR=/home/mitsuhashi/vep-nf/vep_cache
+export CACHE_DIR=$HOME/vep-nf/vep_cache
 
 singularity pull vep_115.sif docker://ensemblorg/ensembl-vep:release_115.0
 singularity exec -B "$CACHE_DIR":/opt/vep/cache vep_115.sif INSTALL.pl -a cf -s homo_sapiens_merged -y GRCh38 -c /opt/vep/cache
