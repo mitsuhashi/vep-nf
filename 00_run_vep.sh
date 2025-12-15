da19279fbd6865e8a76bdd62a92dff49c28974c6
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2025-12-15 16:26:49 +0900

    update 00_run_vep.sh

--- 00_run_vep.sh
+++ 00_run_vep.sh
@@ -2,10 +2,9 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Run Ensembl VEP 115.0 using SLURM and Singularity
-#  --input ${DIR}/../togovar_vcf/grch38/gnomad.v4.1.sv.sites_0000.vcf.gz \
 #  --input ${DIR}/../ensembl-vep/examples/homo_sapiens_GRCh38_mini.vcf.gz \
 #  --input ${DIR}/../togovar_vcf/grch38/clinvar.vcf.gz \
-#  --input ${DIR}/../togovar_vcf/grch38/rs671.vcf.gz \
+#  --input ${DIR}/../togovar_vcf/grch38/gnomad.v4.1.sv.sites.vcf.gz \
 nextflow run ${DIR}/../ensembl-vep \
   -profile slurm,singularity \
   --vep_config ${DIR}/vep.ini \
@@ -13,6 +12,6 @@ nextflow run ${DIR}/../ensembl-vep \
   --cache_version 115 \
   --bin_size 100000 \
   --assembly GRCh38 \
-  --input ${DIR}/../togovar_vcf/grch38/gnomad.v4.1.sv.sites.vcf.gz \
+  --input ${DIR}/../togovar_vcf/grch38/rs671.vcf.gz \
   --merge false \
   --resume
