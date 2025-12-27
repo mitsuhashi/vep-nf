1de52dd46b1dc02fcfd5ad4bad980bc6e101101a
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2025-12-27 19:55:17 +0900

    add CADD, CADD-SV and dbNSFP

--- 00_run_vep.sh
+++ 00_run_vep.sh
@@ -5,13 +5,13 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 #  --input ${DIR}/../ensembl-vep/examples/homo_sapiens_GRCh38_mini.vcf.gz \
 #  --input ${DIR}/../togovar_vcf/grch38/clinvar.vcf.gz \
 #  --input ${DIR}/../togovar_vcf/grch38/gnomad.v4.1.sv.sites.vcf.gz \
+#  --input ${DIR}/../togovar_vcf/grch38/rs671.vcf.gz \
 nextflow run ${DIR}/../ensembl-vep \
   -profile slurm,singularity \
   --vep_config ${DIR}/vep.ini \
   --vep_version 115.0 \
   --cache_version 115 \
-  --bin_size 100000 \
+  --bin_size 5 \
   --assembly GRCh38 \
-  --input ${DIR}/../togovar_vcf/grch38/rs671.vcf.gz \
-  --merge false \
+  --input ${DIR}/../togovar_vcf/grch38/tommo_jsv1.grch38_head50.vcf.gz \
   --resume
--- vep_plugins/install_vep_plugins_cache.sh
+++ vep_plugins/install_vep_plugins_cache.sh
@@ -2,8 +2,47 @@
 #
 set -euo pipefail
 
-mkdir -p ./cache/CADD/v1.7/GRCh38
-wget -O ./cache/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz
-wget -O ./cache/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz.tbi -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz.tbi
-wget -O ./cache/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz
-wget -O ./cache/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz.tbi -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz.tbi
+#
+# CADD
+#
+
+#mkdir -p ./cache/CADD/v1.7/GRCh38
+#wget -O ./cache/CADD/v1.7/GRCh38/GRCh38-v1.7_noanno_prevScored.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38-v1.7_noanno_prevScored.tsv.gz 
+#wget -O ./cache/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz
+#wget -O ./cache/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz.tbi -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/whole_genome_SNVs.tsv.gz.tbi
+#wget -O ./cache/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz
+#wget -O ./cache/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz.tbi -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh38/gnomad.genomes.r4.0.indel.tsv.gz.tbi
+#
+
+#mkdir -p ./cache/CADD/v1.7/GRCh37
+#wget -O ./cache/CADD/v1.7/GRCh37/GRCh37-v1.7_noanno_prevScored.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37-v1.7_noanno_prevScored.tsv.gz
+wget -O ./cache/CADD/v1.7/GRCh37/whole_genome_SNVs.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37/whole_genome_SNVs.tsv.gz
+wget -O ./cache/CADD/v1.7/GRCh37/whole_genome_SNVs.tsv.gz.tbi -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37/whole_genome_SNVs.tsv.gz.tbi
+wget -O ./cache/CADD/v1.7/GRCh37/gnomad.genomes.r4.0.indel.tsv.gz -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37/gnomad.genomes.r4.0.indel.tsv.gz
+wget -O ./cache/CADD/v1.7/GRCh37/gnomad.genomes.r4.0.indel.tsv.gz.tbi -U "Mozilla/5.0" https://krishna.gs.washington.edu/download/CADD/v1.7/GRCh37/gnomad.genomes.r4.0.indel.tsv.gz.tbi
+#
+
+
+
+#
+# CADD-SV GRCh38 only
+#
+#mkdir -p ./cache/CADD-SV/v1.1
+#BASE=https://kircherlab.bihealth.org/download/CADD-SV/v1.1
+
+#wget -c -U "Mozilla/5.0" -P ./cache/CADD-SV/v1.1 \
+#  $BASE/prescored_variants.tsv.gz \
+#  $BASE/prescored_variants.tsv.gz.tbi
+#
+
+
+#
+# dbNSFP
+#
+#wget -O ./cache/dbNSFP/ -U "Mozilla/5.0" https://dist.genos.us/academic/b2fd38/dbNSFP5.3a_grch38.gz
+#wget -O ./cache/dbNSFP/ -U "Mozilla/5.0" https://dist.genos.us/academic/b2fd38/dbNSFP5.3a_grch38.gz.tbi
+#wget -O ./cache/dbNSFP/ -U "Mozilla/5.0" https://dist.genos.us/academic/b2fd38/dbNSFP5.3a_grch38.gz.md5
+
+#wget -O ./cache/dbNSFP/ -U "Mozilla/5.0" https://dist.genos.us/academic/b2fd38/dbNSFP5.3a_grch37.gz
+#wget -O ./cache/dbNSFP/ -U "Mozilla/5.0" https://dist.genos.us/academic/b2fd38/dbNSFP5.3a_grch37.gz.tbi
+#wget -O ./cache/dbNSFP/ -U "Mozilla/5.0" https://dist.genos.us/academic/b2fd38/dbNSFP5.3a_grch37.gz.md5
