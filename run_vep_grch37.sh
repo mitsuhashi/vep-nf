179415053eca27a50b7194ef104ba7cf95240504
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2026-01-16 15:09:37 +0900

    add scripts for running vep for grch37

--- run_vep_grch37.sh
+++ run_vep_grch37.sh
@@ -5,14 +5,7 @@ set -euo pipefail
 # このスクリプトのディレクトリ
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-#VCF=/home/mitsuhashi/togovar_vcf/grch38/gnomad.exomes.v4.1.noinfo.sites.chr22.vcf.bgz
-#VCF=/home/mitsuhashi/togovar_vcf/test/jogo.chr1.vcf.gz
-#VCF=/home/mitsuhashi/togovar_vcf/test/rs671.vcf.bgz
-#VCF=/home/mitsuhashi/togovar_vcf/test/jogo.chr22_100.vcf.gz
-#VCF=/home/mitsuhashi/togovar_vcf/grch38/clinvar.vcf.gz
-#VCF=/home/mitsuhashi/togovar_vcf/grch38/clinvar.vcf.gz
-
-VCF=/home/mitsuhashi/togovar_vcf/grch38
+VCF=/home/mitsuhashi/togovar_vcf/grch37
 
 # 画像キャッシュは共通でOK（容量節約）
 export NXF_SINGULARITY_CACHEDIR=./singularity
@@ -23,9 +16,9 @@ nextflow run nextflow \
   -profile slurm,singularity \
   -work-dir "./work" \
   -resume \
-  --vep_config "${DIR}/vep_grch38.ini" \
+  --vep_config "${DIR}/vep_grch37.ini" \
   --input "$VCF" \
-  --outdir "${DIR}/grch38"
+  --outdir "${DIR}/grch37"
 }
 
 
@@ -37,7 +30,7 @@ run_failed_chunks() {
 nextflow run nextflow \
   -profile slurm,singularity \
   -work-dir "./work" \
-  --vep_config "${DIR}/vep_grch38.ini" \
+  --vep_config "${DIR}/vep_grch37.ini" \
   --input "$VCF" \
   --outdir "${DIR}/grch38" \
   --failed_chunks "${DIR}/grch38/jogo.chr22_100/qc/failed-chunks.tsv" \
